51e9cf3d73d55c3eaabd07315a93c4d5825249ee
oglikap <kroon.rj@gmail.com>
2024-07-11 22:12:19 +0200

    26. importing large dataset

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Student
+from .models import Customer, Employee, Student
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Customer)
+admin.site.register(Employee)
